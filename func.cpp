b7e001786621597738d1898ef7829a71b1b10cd9
grigory karpunin <143996987+sae1012@users.noreply.github.com>
2023-12-24 18:04:12 +0300

    Update func.cpp

--- func.cpp
+++ func.cpp
@@ -3,5 +3,38 @@
 #include "middle_str.h"
 using namespace std;
 bool isLetter(char buk){
-    return ((buk >= 'A' && buk <= 'Z') || (buk >= 'a' && buk <= 'z') || (buk >= 'À' && buk <= 'ß') || (buk >= 'à' && buk <= 'ÿ'));
+    return ((buk >= 'A' && buk <= 'Z') || (buk >= 'a' && buk <= 'z') || (buk >= 'Ã€' && buk <= 'ÃŸ') || (buk >= 'Ã ' && buk <= 'Ã¿'));
+}
+
+
+int isDigit(char symbol){
+    if (symbol >= '0' && symbol <= '9'){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+
+int stringToInt(const string& str) {
+    int res = 0;
+    bool neg = false;
+    int i = 0;
+    if (str[0] == '-') {
+        neg = true;
+        i = 1;
+    }
+    for (; i < str.length(); i++) {
+        if (str[i] >= '0' && str[i] <= '9') {
+            res = res * 10 + (str[i] - '0');
+        } else {
+            return 0;
+        }
+    }
+    if (neg) {
+        res = -res;
+    }
+
+    return res;
 }
